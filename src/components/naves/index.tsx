c944d452a982127cb5dd86cd6124f58f7bd10c61
oJonathanRodrigues <118636531+oJonathanRodrigues@users.noreply.github.com>
2023-06-17 23:27:08 -0300

    Atualização dos icones

--- src/components/naves/index.tsx
+++ src/components/naves/index.tsx
@@ -10,7 +10,7 @@ export default function Naves() {
                 <Vantagem
                     imagem={vantagem1}
                     titulo="Naves"
-                    subtitulo="Aqui você consegue obter os dados das 10 principais naves da saga 🛸"
+                    subtitulo="Aqui você consegue obter os dados das 10 principais naves da saga 🚀"
                     inverter
                 />
             </div>
